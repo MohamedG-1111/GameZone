0af3f9e9f31b8f2e1e296964b0cd8a9a34bd8018
Mohamed <mohamedwwe1111@gmail.com>
2026-04-30 14:15:57 +0300

    Add GenericRepository

---
+++ GameZone/Data/Repositories/GenericRepository.cs
@@ -0,0 +1,80 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameZone.Data.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(ApplicationDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public async Task AddAsync(T entity)
+            => await _dbSet.AddAsync(entity);
+
+        public async Task AddRangeAsync(IEnumerable<T> entities)
+            => await _dbSet.AddRangeAsync(entities);
+
+        public async Task<bool> AnyAsync(Expression<Func<T, bool>> predicate)
+            => await _dbSet.AnyAsync(predicate);
+
+
+
+        public async Task<int> CountAsync(Expression<Func<T, bool>>? predicate)
+        {
+            if (predicate == null)
+            {
+                return await _dbSet.CountAsync();
+            }
+            return await _dbSet.CountAsync(predicate);
+        }
+
+        public void Delete(T entity)
+            => _dbSet.Remove(entity);
+
+        public void DeleteRange(IEnumerable<T> entities)
+            => _dbSet.RemoveRange(entities);
+
+        public void Update(T entity)
+            => _dbSet.Update(entity);
+
+        public async Task<T?> GetByIdAsync(int id)
+            => await _dbSet.FindAsync(id);
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+            => await _dbSet.AsNoTracking().ToListAsync();
+
+        public async Task<IEnumerable<T>> GetAllAsync(
+            Expression<Func<T, bool>>? predicate = null,
+            int? skip = null,
+            int? take = null,
+            params Expression<Func<T, object>>[] includes)
+        {
+            var query = _dbSet.AsNoTracking().AsQueryable();
+
+            if (predicate != null)
+                query = query.Where(predicate);
+
+            foreach (var include in includes)
+                query = query.Include(include);
+
+            if (skip.HasValue) query = query.Skip(skip.Value);
+            if (take.HasValue) query = query.Take(take.Value);
+
+            return await query.ToListAsync();
+        }
+
+        public IQueryable<T> GetAsQuery(bool noTracking = true)
+        {
+            var query = _dbSet.AsQueryable();
+            return noTracking ? query.AsNoTracking() : query;
+        }
+
+
+    }
+}
---
+++ GameZone/Data/Repositories/IGenericRepository.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq.Expressions;
+
+namespace GameZone.Data.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        public Task AddAsync(T entity);
+
+        public Task AddRangeAsync(IEnumerable<T> entities);
+
+
+        public Task<T?> GetByIdAsync(int id);
+
+        public void Update(T entity);
+
+        public void Delete(T entity);
+
+        public void DeleteRange(IEnumerable<T> entities);
+
+        Task<int> CountAsync(Expression<Func<T, bool>>? predicate);
+        public Task<bool> AnyAsync(Expression<Func<T, bool>> predicate);
+
+        IQueryable<T> GetAsQuery(bool NoTracking = true);
+
+        public Task<IEnumerable<T>> GetAllAsync();
+        Task<IEnumerable<T>> GetAllAsync(
+                    Expression<Func<T, bool>>? predicate = null,
+                    int? skip = null,
+                    int? take = null,
+                   params Expression<Func<T, object>>[] includes);
+
+    }
+}
